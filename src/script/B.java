07ee2dab2a2c1583f56f34cd7f148965a2988085
Megha189 <32758865+Megha189@users.noreply.github.com>
2017-11-17 11:23:17 +0530

    B class is created

---
+++ src/script/B.java
@@ -0,0 +1,13 @@
+package script;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class B {
+	@Test
+	public void call()
+	{
+		Reporter.log("hi b class is creted",true);
+	}
+
+}
